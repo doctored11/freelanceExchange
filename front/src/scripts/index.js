7ff49992c35a7c0da4cf5168a26afb6db8a3d78b
DR0PDATABASE <waifucorp@icloud.com>
2023-10-05 22:15:14 +0300

    10:15

--- front/src/scripts/index.js
+++ front/src/scripts/index.js
@@ -18,11 +18,11 @@ setInterval(() => {
 import { User } from './User.js'
 let user = User.load()
 if (user != null) {
-    const id = user.id//надо имя
-    document.querySelector('#name').innerText = id;
+    document.querySelector('#nav_name').innerText = user.bio;
+    console.log(user.id, user.bio)
+
 }
 else {
-    document.querySelector('#name').setAttribute("href", "./registration.html")
+    document.querySelector('#nav__name').setAttribute("href", "./registration.html")
 }
-
 
--- front/src/scripts/reg.js
+++ front/src/scripts/reg.js
@@ -30,6 +30,7 @@ getServices('../data/base.json', usersData)
 
 
 // желательно кнопку блокировать пока usersData не получен todo
+let email;
 function handleFormSubmit(event) {
     if (sendBtn.disabled) {
         document.querySelector('#registration-form__error').value = 'fatal error'
@@ -44,12 +45,12 @@ function handleFormSubmit(event) {
     else {// если регистрируемся то то
         //все поля
     }
-    // personName = inputName.value;
-    // lastName = inputLastName.value;
-    // pass = inputPass.value;
-    // email = inputEmail.value;
-    // const client = flagClient;
-    // const implementer = !flagClient;
+    personName = inputName.value;
+    lastName = inputLastName.value;
+    pass = inputPass.value;
+    email = inputEmail.value;
+    const client = flagClient;
+    const implementer = !flagClient;
 
     // Ваши данные теперь доступны в переменных name, email и roles
     // const id = usersData.length + 1; // когда будет Бд
@@ -71,3 +72,84 @@ function handleFormSubmit(event) {
 
 
 }
+//Полетели обрабатывать
+
+const signInBtn = document.querySelector('.signin-btn');
+const signUnBtn = document.querySelector('.signup-btn');
+const sellerBtn = document.querySelector('.seller-btn');
+const coderBtn = document.querySelector('.coder-btn');
+let flagLog = true;
+let flagClient = true;
+
+const inputName = document.querySelector('#name');
+const inputLastName = document.querySelector('#lastname');
+const inputEmail = document.querySelector('#email');
+const inputPhone = document.querySelector('#phone');
+const inputPass = document.querySelector('#password');
+const sendBtn = document.querySelector('.registration-form__submit-button');
+let pass = '';
+let personName = '';
+let lastName = '';
+let phone = '';
+
+//кнопка что ты регистрируешься
+signUnBtn.addEventListener("click", f => {
+    if (flagLog) {
+        signUnBtn.classList.add('active');
+        signInBtn.classList.remove('active');
+        let regLabels = document.querySelectorAll('.reg-group')
+        regLabels.forEach((element) => element.style.display = 'block');
+        flagLog = false;
+    }
+    checkBtn()
+})
+//кнопка что ты входишь в систему
+signInBtn.addEventListener("click", f => {
+    if (!flagLog) {
+        signUnBtn.classList.remove('active');
+        signInBtn.classList.add('active');
+        let regLabels = document.querySelectorAll('.reg-group')
+        regLabels.forEach((element) => element.style.display = 'none');
+        flagLog = true;
+    }
+})
+//кнопка что ты разработчик
+coderBtn.addEventListener("click", f => {
+    if (flagClient) {
+        sellerBtn.classList.remove('active');
+        coderBtn.classList.add('active');
+        flagClient = false;
+    }
+})
+//кнопка что ты покупатель
+sellerBtn.addEventListener("click", f => {
+    if (!flagClient) {
+        sellerBtn.classList.add('active');
+        coderBtn.classList.remove('active');
+        flagClient = true;
+    }
+})
+function checkBtn() {
+    personName = inputName.value;
+    lastName = inputLastName.value;
+    pass = inputPass.value;
+    email = inputEmail.value;
+    phone = inputPhone.value;
+    if ((flagLog && pass.length > 0 && email.length > 0) || (!flagLog && pass.length > 0 && lastName.length > 0 && personName.length > 0 && email.length > 0 && phone.length > 0)) {
+        sendBtn.disabled = false;
+        sendBtn.classList.add('active');
+        // Находим кнопку и назначаем обработчик события на отправку формы
+        document.querySelector('.registration-form__submit-button').addEventListener('click', handleFormSubmit);
+    }
+    else {
+        sendBtn.disabled = true;
+        sendBtn.classList.remove('active')
+        // Находим кнопку и назначаем обработчик события на отправку формы
+    }
+}
+document.querySelector('#registration-form__error').innerText = 'fatal error'; //Текст об ошибке
+inputName.addEventListener('input', checkBtn);
+inputLastName.addEventListener('input', checkBtn);
+inputPass.addEventListener('input', checkBtn);
+inputPhone.addEventListener('input', checkBtn);
+inputEmail.addEventListener('input', checkBtn);
